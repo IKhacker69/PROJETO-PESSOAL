d6a0f01e327f7c9fd5a960efe7d173086add3976
MRafaelM <inacio.kunz@gmail.com>
2025-06-13 15:40:19 -0300

    Vinculando banco de dados

--- template/login.py
+++ template/login.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, redirect, request, flash
-import json
+import mysql.connector
+
+# Conexão com o banco de dados
+conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='usuarios'
+)
+cursor = conexao.cursor(dictionary=True)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'INACIO'
@@ -13,39 +22,31 @@ def login_page():
     if request.method == 'POST':
         nome = request.form.get('nome')
         senha = request.form.get('senha')
-        with open('usuarios.json') as usuariosTemp:
-            usuarios = json.load(usuariosTemp)
-            for usuario in usuarios:
-                if nome == 'Admin' and senha == '123456':
-                    return redirect('/admin')
-                if usuario['nome'] == nome and usuario['senha'] == senha:
-                    return redirect('/home')
+
+        cursor.execute("SELECT * FROM usuarios WHERE nome = %s AND senha = %s", (nome, senha))
+        usuario = cursor.fetchone()
+
+        if nome == 'Admin' and senha == '123456':
+            return redirect('/admin')
+
+        if usuario:
+            return redirect('/home')
+        else:
             flash('Usuário ou senha incorretos!')
             return redirect('/login')
 
+    return render_template('login.html')
+
 @app.route('/cadastrarUsuario', methods=['POST'])
 def cadastrarUsuario():
-    user = [ ]
     nome = request.form.get('nome')
     senha = request.form.get('senha')
-    user = [
-        {
-            'nome': nome,
-            'senha': senha
-        }
-    ]
-    with open('usuarios.json') as usuariosTemp:
-        usuarios = json.load(usuariosTemp)
-    
-    usuarioNovo = user + usuarios
 
-    with open('usuarios.json' , 'w') as gravarTemp:
-        json.dump(usuarioNovo, gravarTemp, indent=4)
+    cursor.execute("INSERT INTO usuarios (nome, senha) VALUES (%s, %s)", (nome, senha))
+    conexao.commit()
+
     flash('Usuário cadastrado com sucesso!')
-
     return redirect('/login')
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
