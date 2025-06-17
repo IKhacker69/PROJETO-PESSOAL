a9caabadbc84a663585faff6c9d88f209399ca32
MRafaelM <inacio.kunz@gmail.com>
2025-06-17 10:30:04 -0300

    organizando pastas

--- home.py
+++
@@ -1,6 +0,0 @@
-# class agendamentos:More actions
-#     def __init__(self, nome, categoria, data, hora):
-#         self.nome = nome
-#         self.categoria = categoria
-#         self.data = data
-#         self.hora = hora    
