a2958128eb1a8be3261c485cbffde914f3c3e5d0
Inacio Kunz <inacio.kunz@gmail.com>
2025-07-02 17:47:30 -0300

    opa

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import pymysql
 
 conexao = pymysql.connect(
@@ -50,8 +50,8 @@ def novo():
             (nome, categoria, data, hora)
         )
         conexao.commit()
-    agendamentos = get_agendamentos()
-    return render_template('novo.html', agendamentos=agendamentos)
+        agendamentos = get_agendamentos()
+    return redirect('/agendamentos')
 
 if __name__ == '__main__':
     app.run(debug=True)
